c3b8dfdf0f829ec9a3305a54330ee3ca512b306a
Fernando Valente <fernando@fvalente.org>
2014-08-28 20:47:34 +0300

    Create main.m

---
+++ TaskManager/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TaskManager
+//
+//  Created by Fernando Valente on 8/28/14.
+//  Copyright (c) 2014 Fernando Valente. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
